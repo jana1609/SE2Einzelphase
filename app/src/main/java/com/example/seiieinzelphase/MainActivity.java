1808d119fa563bd0631dd75e89bf99e8aa4b344a
Jana <j.kampfmeier@live.de>
2021-03-05 12:06:03 +0100

    Added Server Connection
    Fully functional, write and read on server socket

--- app/src/main/java/com/example/seiieinzelphase/MainActivity.java
+++ app/src/main/java/com/example/seiieinzelphase/MainActivity.java
@@ -7,6 +7,13 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,8 +22,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    String output;
+
     public void sendServer(View view){
-        // do something
+        // send input to server and show output
+
+        // get Layout Elements
+        TextView tvAntwort = findViewById(R.id.tvAntwort);
+        EditText input = findViewById(R.id.input);
+
+        // config Thread
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Socket socket = new Socket("se2-isys.aau.at",53212);
+                    PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+                    printWriter.println(input.getText().toString());
+                    printWriter.flush();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    output = bufferedReader.readLine();
+                    printWriter.close();
+                    socket.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                    output = "Server error\nPlease try again";
+                }
+            }
+        });
+
+        // run thread
+        thread.start();
+        try {
+            thread.join();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+            output = "Server error\nPlease try again";
+        }
+
+        // output result
+        tvAntwort.setText(output);
     }
 
     public void calculation(View view){
@@ -33,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
             altQuersumme += Math.pow(-1, i) * Character.getNumericValue(inputString.charAt(inputString.length()-1-i));
         }
         // Quersumme gerade oder ungerade
-        String output = "Die alternierende Quersumme "+altQuersumme+" von "+inputString+" ist ";
+        output = "Die alternierende Quersumme "+altQuersumme+" von "+inputString+" ist ";
         if(altQuersumme % 2 == 0) output += "gerade";
         else output += "ungerade";
 
