c0f56b11a4581279c6ed32f1b37ad5d02d4badb3
Jana <j.kampfmeier@live.de>
2021-03-05 10:53:06 +0100

    Added onClick on Button

--- app/src/main/java/com/example/seiieinzelphase/MainActivity.java
+++ app/src/main/java/com/example/seiieinzelphase/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.seiieinzelphase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calculation(View view){
+        // do something
+        TextView tvAntwort = (TextView) findViewById(R.id.tvAntwort);
+        tvAntwort.setText("Button was clicked!!!");
     }
 }
